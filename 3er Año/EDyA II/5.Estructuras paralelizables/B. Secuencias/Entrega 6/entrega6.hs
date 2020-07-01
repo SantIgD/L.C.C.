7c34590690fed73505ff684963debadf8aebadb4
Santiago Ignacio Demagistris <demagistris.santiago@gmail.com>
2020-07-01 18:13:06 -0300

    [EDyA II] [Entrega6] [EJ 4] Prueba de implementacion

--- 3er Año/EDyA II/5.Estructuras paralelizables/B. Secuencias/Entrega 6/entrega6.hs
+++ 3er Año/EDyA II/5.Estructuras paralelizables/B. Secuencias/Entrega 6/entrega6.hs
@@ -1,6 +1,7 @@
 import Seq
 import ArrSeq
 import Par ((|||))
+import qualified Arr as A
 -- Practica 6 --
 
 -- Ejercicio 4
@@ -14,7 +15,7 @@ matchParen seqParen = bienAnidados (scanS (+) 0 (mapS f seqParen))
 
 
 bienAnidados :: Seq s => (s Int,Int) -> Bool
-bienAnidados (xs,0) = menosClose  (map (\x -> x >= 0) xs)
+bienAnidados (xs,0) = menosClose  (mapS (\x -> x >= 0) xs)
 bienAnidados _      = False
 
 
@@ -22,21 +23,24 @@ f :: Paren -> Int
 f Close = (-1)
 f _     = 1
 
-menosClose :: Seq s => s Int -> Bool
+menosClose :: Seq s => s Bool -> Bool
 menosClose seqInt = reduceS (&&) True seqInt
 
+parentesis1 = [Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close]
+parentesis2 = [Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close]
+parentesis3 = [Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Open,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close,Close]
 
--- Ejercicio 6
+-- -- Ejercicio 6
 
-multiplos seqParen = reduceS (+) 0 
-                                (tabulateS
-                                  (\i -> let subSeq = dropS seqParen i 
-                                         in reduceS (+) 0 (map (k (nthS subSeq 0) xs) (dropS subSeq 1))
-                                  ) 
-                                    l
-                                ) seqParen
-                 where 
-                   l = (length seqParen) - 1
+-- multiplos seqParen = reduceS (+) 0 
+--                                 (tabulateS
+--                                   (\i -> let subSeq = dropS seqParen i 
+--                                          in reduceS (+) 0 (map (k (nthS subSeq 0) xs) (dropS subSeq 1))
+--                                   ) 
+--                                     l
+--                                 ) seqParen
+--                  where 
+--                    l = (length seqParen) - 1
                 
-k :: Int -> Int -> Int
-k x y = if mod x y == 0 then 1 else 0
+-- k :: Int -> Int -> Int
+-- k x y = if mod x y == 0 then 1 else 0
