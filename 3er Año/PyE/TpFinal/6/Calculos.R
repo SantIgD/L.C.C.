4c8ab809f4470924fd71c8e4573304a882b0af7f
unknown <demagistris.santiago@gmail.com>
2020-08-22 17:45:27 -0300

    [PyE] [Tp Final] ej 6 simulacion hecha.

--- 3er Año/PyE/TpFinal/6/Calculos.R
+++ 3er Año/PyE/TpFinal/6/Calculos.R
@@ -12,6 +12,47 @@ Xg = c(1/7,1,2,3,4,5,6,7)
 
 P = cargarMatriz(P)
 
+
+contadorPaginas = c(1:7)
+
+probabilidad= simular(100)
+
+
+simular<- function(n){
+
+  contadorPaginas= cerarVector(contadorPaginas)
+  
+  paginaInicial = sample(1:7,1) # Inicio de manera aleatoria
+  print("La pagina inicial es")
+  print(paginaInicial)
+  paginaActual = paginaInicial
+  
+  for (i in (1:n)){
+    paginaActual = surfear(paginaActual)
+    contadorPaginas[paginaActual] = contadorPaginas[paginaActual] + 1 
+    print("La pagina actual es")
+    print(paginaActual)
+    print("Contador")
+    print(contadorPaginas)
+  }
+  return (contadorPaginas/n)
+  
+}
+
+surfear <- function (x){
+  paginaActual = obtenerFila(x)
+  proximaPagina = paginaActual[elegirProximaPagina(paginaActual)]
+  return (proximaPagina)
+}
+
+
+elegirProximaPagina <-function(x){
+  unoEntre = length(x)-1
+  return (sample(1:unoEntre,1)+1)
+}
+
+
+
 cargarMatriz <- function(P){
   P = cerarMatriz(P)
   indiceFilas = 0
@@ -27,7 +68,12 @@ cargarMatriz <- function(P){
   return (P)
 }
 
-
+cerarVector <- function(v){
+  for (i in (1:7)){
+    v[i] = 0
+  }
+  return (v)
+}
 cerarMatriz <- function(P){
   for (i in (1:7)){
     for (j in (1:7)){
@@ -35,6 +81,30 @@ cerarMatriz <- function(P){
     }
   }
   return (P)
+}
+
+obtenerFila <- function(x){
+  if (x == 1){
+    return (Xa)
+  }
+  if (x == 2){
+    return (Xb)
+  }
+  if (x == 3){
+    return (Xc)
+  }
+  if (x == 4){
+    return (Xd)
+  }
+  if (x == 5){
+    return (Xe)
+  }
+  if (x == 6){
+    return (Xf)
+  }
+  if (x == 7){
+    return (Xg)
+  }
 }
 
 cargarFila <- function(P,x,fila){
